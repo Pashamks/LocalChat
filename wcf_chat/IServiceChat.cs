fd37537c48983fd0eed0d96ee25123e42ffc8453
Pashamks <74491257+Pashamks@users.noreply.github.com>
2021-07-23 20:48:49 +0300

    first commit

---
+++ wcf_chat/IServiceChat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IServerChatCallBack))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        int Connect(string name);// here we have to wait for serwer answer
+        [OperationContract]
+        void DisConnect(int id);
+        [OperationContract(IsOneWay = true)]//we don't need to wait for answer from serwer
+        void SentMsg(string msg, int id);
+    }
+
+    public interface IServerChatCallBack
+    {
+        [OperationContract(IsOneWay = true)]
+        void MsgCallBack(string msg);
+    }
+}
---
+++ wcf_chat/ServiceChat.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+
+        int nextId = 1;
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser() { 
+                ID = nextId, 
+                Name = name, 
+                operationContext = OperationContext.Current};
+
+            ++nextId;
+            SentMsg(user.Name + " connected to chat !", 0);
+            users.Add(user);
+
+            return user.ID;
+        }
+
+        public void DisConnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+            if(user!=null)
+            {
+                users.Remove(user);
+                SentMsg(user.Name + " disconnected !", 0);
+            }
+        }
+
+        public void SentMsg(string msg, int id)
+        {
+            string answer;
+            foreach(var item in users)
+            {
+                answer = DateTime.Now.ToShortTimeString();
+                var user = users.FirstOrDefault(i => i.ID == id);
+                if (user != null)
+                {
+                    answer += " " + user.Name + " ";
+                }
+                answer += msg;
+                item.operationContext.GetCallbackChannel<IServerChatCallBack>().MsgCallBack(answer);
+            }
+        }
+    }
+}
